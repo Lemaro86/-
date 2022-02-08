2787d4a02aae7cad7b3a08197c754314b6073978
lemarolemarov <rponomarev@belkacar.ru>
2022-02-08 20:51:59 +0300

    gb-2-1

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const name='Roman'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App myName={name} topPosition={'100px'} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
